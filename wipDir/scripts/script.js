d13b42a9b5528766a4f66e1bea270a7340cd7b43
Colin Kennedy <colinkennedy@hotmail.com>
2016-11-17 10:30:01 -0700

    Testing event attachment

--- wipDir/scripts/script.js
+++ wipDir/scripts/script.js
@@ -1,3 +1,13 @@
-document.addEventListener("scroll", function(){
-    alert ("hi");
-});
+var header = document.getElementsByClassName("page-header");
+var projectName = document.getElementsByClassName("project-name");
+var tagline = document.getElementsByClassName("project-tagline");
+
+// document.addEventListener("scroll", function(){
+//     if (document.scro)
+// });
+
+document.onscroll = function () {
+    if (document.scrollingElement.scrollTop > 100){
+        console.log("shrink");
+    }
+}
