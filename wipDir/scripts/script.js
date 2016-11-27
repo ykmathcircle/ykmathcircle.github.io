f6a682f4560712abd5cf0d1e6e978ff64ffc5c1c
Colin Kennedy <colinkennedy@hotmail.com>
2016-11-26 17:10:45 -0700

    Improve fixed-header script

--- wipDir/scripts/script.js
+++ wipDir/scripts/script.js
@@ -1,7 +1,17 @@
-var header = document.getElementsByClassName("page-header").item(0);
+var header      = document.getElementsByClassName("page-header").item(0);
 var projectName = document.getElementsByClassName("project-name")[0];
-var tagline = document.getElementsByClassName("project-tagline")[0];
-var main = document.getElementsByClassName("main-content")[0];
+var tagline     = document.getElementsByClassName("project-tagline")[0];
+var main        = document.getElementsByClassName("main-content")[0];
+var logo        = document.getElementsByClassName("logo")[0];
+
+var shrinkies = [];
+
+shrinkies.push(document.getElementsByClassName("page-header").item(0));
+shrinkies.push(document.getElementsByClassName("project-name")[0]);
+shrinkies.push(document.getElementsByClassName("project-tagline")[0]);
+shrinkies.push(document.getElementsByClassName("main-content")[0]);
+shrinkies.push(document.getElementsByClassName("logo")[0]);
+
 
 // main.setAttribute("padding-top", header.getAttribute("height"));
 main.setAttribute("style", "padding-top: " + (header.clientHeight + 25 ) + "px");
@@ -13,13 +23,19 @@ main.setAttribute("style", "padding-top: " + (header.clientHeight + 25 ) + "px")
 
 document.onscroll = function () {
     if (document.scrollingElement.scrollTop > 100){
-        console.log("shrink");
-        header.classList.add("shrink");
-        projectName.classList.add("shrink");
-        tagline.classList.add("shrink");
+        // console.log("shrink");
+        // header.classList.add("shrink");
+        // projectName.classList.add("shrink");
+        // tagline.classList.add("shrink");
+        shrinkies.forEach(function(el){
+            el.classList.add("shrink");
+        });
     } else {
-        header.classList.remove("shrink");
-        projectName.classList.remove("shrink");
-        tagline.classList.remove("shrink");
+        // header.classList.remove("shrink");
+        // projectName.classList.remove("shrink");
+        // tagline.classList.remove("shrink");
+        shrinkies.forEach(function(el){
+            el.classList.remove("shrink");
+        });
     }
 }
