6c624f0068f469ab66de5b54887f7de322152e94
Colin Kennedy <colinkennedy@hotmail.com>
2016-11-27 14:16:13 -0700

    Update script

--- wipDir/scripts/script.js
+++ wipDir/scripts/script.js
@@ -1,8 +1,8 @@
-var header      = document.getElementsByClassName("page-header").item(0);
+var header = document.getElementsByClassName("page-header").item(0);
 var projectName = document.getElementsByClassName("project-name")[0];
-var tagline     = document.getElementsByClassName("project-tagline")[0];
-var main        = document.getElementsByClassName("main-content")[0];
-var logo        = document.getElementsByClassName("logo")[0];
+var tagline = document.getElementsByClassName("project-tagline")[0];
+var main = document.getElementsByClassName("main-content")[0];
+var logo = document.getElementsByClassName("logo")[0];
 
 var shrinkies = [];
 
@@ -13,8 +13,10 @@ shrinkies.push(document.getElementsByClassName("main-content")[0]);
 shrinkies.push(document.getElementsByClassName("logo")[0]);
 
 
+
+
 // main.setAttribute("padding-top", header.getAttribute("height"));
-main.setAttribute("style", "padding-top: " + (header.clientHeight + 25 ) + "px");
+main.setAttribute("style", "padding-top: " + (header.clientHeight + 25) + "px");
 
 
 // document.addEventListener("scroll", function(){
@@ -22,20 +24,24 @@ main.setAttribute("style", "padding-top: " + (header.clientHeight + 25 ) + "px")
 // });
 
 document.onscroll = function () {
-    if (document.scrollingElement.scrollTop > 100){
+    if (document.scrollingElement.scrollTop > 100) {
         // console.log("shrink");
         // header.classList.add("shrink");
         // projectName.classList.add("shrink");
         // tagline.classList.add("shrink");
-        shrinkies.forEach(function(el){
-            el.classList.add("shrink");
+        shrinkies.forEach(function (el) {
+            if (el) {
+                el.classList.add("shrink");
+            }
         });
     } else {
         // header.classList.remove("shrink");
         // projectName.classList.remove("shrink");
         // tagline.classList.remove("shrink");
-        shrinkies.forEach(function(el){
-            el.classList.remove("shrink");
+        shrinkies.forEach(function (el) {
+            if (el) {
+                el.classList.remove("shrink");
+            }
         });
     }
 }
