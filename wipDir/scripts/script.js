2e0f4a2b792cb29d11e940fede589f7ca35d5f7b
Colin Kennedy <colinkennedy@hotmail.com>
2016-11-27 22:12:50 -0700

    Add forEach to shrinkers HTMLCollection

--- wipDir/scripts/script.js
+++ wipDir/scripts/script.js
@@ -4,14 +4,22 @@ main.setAttribute("style", "padding-top: " + (header.clientHeight + 25) + "px");
 
 shrinkers = document.getElementsByClassName("shrinker");
 
+
+shrinkers.forEach = function(f){
+    for (var i = 0; i<shrinkers.length; i++){
+        f(shrinkers[i]);
+    }
+}
+
+
 document.onscroll = function () {
     if (document.scrollingElement.scrollTop > 100) {
         shrinkers.forEach(function(el){
             el.classList.add("shrink");
-        });
+        })
     } else {
         shrinkers.forEach(function(el){
             el.classList.remove("shrink");
-        });
+        })
     }
 }
