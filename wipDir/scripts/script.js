0af2e2b4720bd21fe97f03cb82854bfe99dda2ba
Colin Kennedy <colinkennedy@hotmail.com>
2016-11-27 22:17:39 -0700

    Explicit loop over shrinkers list

--- wipDir/scripts/script.js
+++ wipDir/scripts/script.js
@@ -5,21 +5,17 @@ main.setAttribute("style", "padding-top: " + (header.clientHeight + 25) + "px");
 shrinkers = document.getElementsByClassName("shrinker");
 
 
-shrinkers.forEach = function(f){
-    for (var i = 0; i<shrinkers.length; i++){
-        f(shrinkers[i]);
-    }
-}
+
 
 
 document.onscroll = function () {
     if (document.scrollingElement.scrollTop > 100) {
-        shrinkers.forEach(function(el){
-            el.classList.add("shrink");
-        })
+        for (var i=0; i<shrinkers.length; i++){
+            shrinkers[i].classList.add("shrink");
+        }
     } else {
-        shrinkers.forEach(function(el){
-            el.classList.remove("shrink");
-        })
+        for (var i=0; i<shrinkers.length; i++){
+            shrinkers[i].classList.remove("shrink");
+        }
     }
 }
