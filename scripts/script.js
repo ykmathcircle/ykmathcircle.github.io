7df72fb3229ec9988d884f10eca50c349ef13133
Colin Kennedy <colinkennedy@hotmail.com>
2016-11-29 12:42:03 -0700

    Try parallax scrolling on header

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 var fixedHead = document.getElementById("fixedHeader");
+var topHead = document.getElementById("topHeader");
 
 document.onscroll = function () {
+    topHead.style.top = (- document.scrollingElement.scrollTop / 2 ) + "";
+
     if (document.scrollingElement.scrollTop > 600) {
         fixedHead.classList.add("page-header-fixed-visible");
     }
